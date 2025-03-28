1cdcf9c9034d3566d311bffe4c093f5da202ffd4
ABHISHEK KUMAR <130243689+ABHISHEK22KGP@users.noreply.github.com>
2025-03-28 21:48:26 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-
-const connection = mongoose.connect('mongodb+srv://ABHISHEK22KGP2026:BHI9Jdk1zO3juE4U@cluster0.srcbnno.mongodb.net/1st_backend').then(()=>{
+const MONGO_URI = process.env.MONGO_URI
+const connection = mongoose.connect(MONGO_URI).then(()=>{
     console.log("Connected To DataBase")
 })
 module.exports = connection 
